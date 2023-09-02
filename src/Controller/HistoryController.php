d20545c9bc9552e60420b2cfaf8f80e48bca9e2c
Aniekan Offiong <ekanoffiong@gmail.com>
2023-09-02 20:00:12 +0200

    Added method to save history to database

---
+++ src/Controller/HistoryController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\History;
+use Doctrine\ORM\EntityManagerInterface;
+use DateTime;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class HistoryController  extends AbstractController {
+
+    #[Route('/exchange/values', methods: ['POST'])]
+    public function exchangeValues(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
+
+        $first = $data['first'];
+        $second = $data['second'];
+
+        $history = new History();
+        $history->setFirstInValue($first);
+        $history->setSecondInValue($second);
+        $history->setDateOfCreation(new DateTime('now'));
+        $history->setDateOfUpdate(new DateTime('now'));
+
+        $entityManager->persist($history);
+        $entityManager->flush();
+
+        $first = $data['second'];
+        $second = $data['first'];
+
+        $history->setFirstOutValue($first);
+        $history->setSecondOutValue($second);
+        $history->setDateOfUpdate(new DateTime('now'));
+
+        $entityManager->persist($history);
+        $entityManager->flush();
+
+        return $this->json(['first' => $first, 'second' => $second]);
+    }
+}
